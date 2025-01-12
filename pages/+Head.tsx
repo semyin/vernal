786be047017d2e9469a6004c59b14a58587a5f47
semyin <semyin@163.com>
2025-01-12 16:21:22 +0800

    feat: rest css

---
+++ pages/+Head.tsx
@@ -0,0 +1,16 @@
+export { Head };
+
+import  '../assets/css/reset.css'
+
+function Head() {
+  return (
+    <>
+      <meta name="generator" content="vavite" />
+
+      <meta name="project" content="Vernal" />
+
+      <meta name="template" content="nestjs-vike" />
+
+    </>
+  );
+}
