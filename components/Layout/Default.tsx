ff86e2782cbf0731969156ef54c875c25070ff7b
semyin <semyin@kplocker.com>
2025-01-13 15:42:02 +0800

    fix

--- components/Layout/Default.tsx
+++ components/Layout/Default.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Header from "../Header/Header";
-import Footer from "../Footer/Footer";
+import Header from "#root/components/Header/Header";
+import Footer from "#root/components/Footer/Footer";
 
 export {
   Layout
