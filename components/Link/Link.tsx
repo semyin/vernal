f3776a84b7ee5e2972229cc4901c46c5e167dc15
semyin <semyin@163.com>
2025-02-04 17:31:22 +0800

    fix: Link with inline style

--- components/Link/Link.tsx
+++ components/Link/Link.tsx
@@ -1,13 +1,14 @@
 export { Link }
 
+import { CSSProperties } from 'react';
 import { usePageContext } from 'vike-react/usePageContext'
 
-function Link({ href, children }: { href: string; children: string }) {
+function Link({ href, children, style }: { href: string; children: string, style?: CSSProperties| undefined }) {
   const pageContext = usePageContext()
   const { urlPathname } = pageContext
   const isActive = href === '/' ? urlPathname === href : urlPathname.startsWith(href)
   return (
-    <a href={href} className={isActive ? 'is-active' : undefined}>
+    <a style={{ ...style }} href={href} className={isActive ? 'is-active' : undefined}>
       {children}
     </a>
   )
