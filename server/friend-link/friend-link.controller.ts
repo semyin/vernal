98a80b9eb7dbefd88f5190d12d123316188a218b
semyin <semyin@kplocker.com>
2025-02-17 17:41:15 +0800

    fix: friend link controller method order

--- server/friend-link/friend-link.controller.ts
+++ server/friend-link/friend-link.controller.ts
@@ -1,20 +1,22 @@
-import { Controller, Get, Post, Body, Param, Put, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Put, Delete, UseGuards } from '@nestjs/common';
 import { FriendLinkService } from './friend-link.service';
 import { CreateFriendLinkDto } from './dto/create-friend-link.dto';
 import { UpdateFriendLinkDto } from './dto/update-friend-link.dto';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 
 @Controller('friend-links')
 export class FriendLinkController {
   constructor(private readonly friendLinkService: FriendLinkService) {}
 
-  @Post()
-  async create(@Body() createFriendLinkDto: CreateFriendLinkDto) {
-    return this.friendLinkService.create(createFriendLinkDto);
-  }
-
   @Get()
   async findAll() {
     return this.friendLinkService.findAll();
+  }
+
+  @Post()
+  @UseGuards(JwtAuthGuard)
+  async create(@Body() createFriendLinkDto: CreateFriendLinkDto) {
+    return this.friendLinkService.create(createFriendLinkDto);
   }
 
   @Get(':id')
@@ -23,11 +25,13 @@ export class FriendLinkController {
   }
 
   @Put(':id')
+  @UseGuards(JwtAuthGuard)
   async update(@Param('id') id: number, @Body() updateFriendLinkDto: UpdateFriendLinkDto) {
     return this.friendLinkService.update(id, updateFriendLinkDto);
   }
 
   @Delete(':id')
+  @UseGuards(JwtAuthGuard)
   async delete(@Param('id') id: number) {
     return this.friendLinkService.delete(id);
   }
