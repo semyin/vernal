416716297b7cbd7c0262ef7ceff02362aaaf91aa
semyin <semyin@kplocker.com>
2025-02-18 12:01:00 +0800

    file controller

--- server/file/file.controller.ts
+++ server/file/file.controller.ts
@@ -8,10 +8,14 @@ import {
   Get,
   Query,
   Body,
+  UseGuards,
 } from "@nestjs/common";
 import { FileInterceptor } from "@nestjs/platform-express";
 import { FileService } from "./file.service";
 import { File } from "./file.entity";
+import { DefaultValuePipe, ParseIntPipe } from "@nestjs/common";
+import { Pagination } from "../../types/pagination.interface";
+import { JwtAuthGuard } from "../auth/jwt-auth.guard";
 import { Multer } from "multer";
 
 @Controller("files")
@@ -19,6 +23,7 @@ export class FileController {
   constructor(private readonly fileService: FileService) {}
 
   @Post("upload")
+  @UseGuards(JwtAuthGuard)
   @UseInterceptors(
     FileInterceptor("file", {
       limits: {
@@ -39,12 +44,23 @@ export class FileController {
   }
 
   @Delete(":id")
+  @UseGuards(JwtAuthGuard)
   async deleteFile(@Param("id") id: number): Promise<void> {
     return this.fileService.deleteFile(id);
   }
 
   @Get()
+  @UseGuards(JwtAuthGuard)
   async getFilesByType(@Query("type") type: string): Promise<File[]> {
     return this.fileService.getFilesByType(type);
+  }
+  @Get()
+  @UseGuards(JwtAuthGuard)
+  getFileList(
+    @Query("page", new DefaultValuePipe(1), ParseIntPipe) page: number = 1,
+    @Query("limit", new DefaultValuePipe(20), ParseIntPipe) limit: number = 20,
+    @Query("type") type?: string
+  ): Promise<Pagination<File>> {
+    return this.fileService.getFileList({ page, limit }, type);
   }
 }
