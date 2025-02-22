ad0aeafc122b8f2943b0ae4663aed92fd14b33f8
semyin <semyin@kplocker.com>
2025-02-22 15:51:52 +0800

    fix: un used

--- server/auth/auth.service.ts
+++ server/auth/auth.service.ts
@@ -1,8 +1,8 @@
-import { Injectable, UnauthorizedException } from "@nestjs/common";
+import { Injectable } from "@nestjs/common";
 import { JwtService } from "@nestjs/jwt";
 import { Response } from "express";
-import { UserService } from "../user/user.service";
 import { InvalidCredentialsException } from "../common/exceptions/invalid-credentials.exception";
+import { UserService } from "../user/user.service";
 
 @Injectable()
 export class AuthService {
