dd6e3c0f4e2c496a08bc4caaba332d07ae722258
semyin <semyin@163.com>
2025-02-23 08:00:00 +0800

    files type spit

--- api/files/index.ts
+++ api/files/index.ts
@@ -1,22 +1,11 @@
 import request from "#root/utils/request";
 import { File } from "buffer";
+import { UploadResponse } from "./type";
 
 interface UploadBody {
   file: File;
   type: string;
 }
 
-export interface UploadResponse {
-  id: number;
-  filename: string;
-  originalname: string;
-  mimetype: string;
-  size: number;
-  type: string;
-  url: string;
-  cosKey: string;
-  createdAt: string;
-  updatedAt: string;
-}
 export const uploadFile = async (data: FormData): Promise<UploadResponse> =>
   request.post("/files/upload", data);
---
+++ api/files/type.ts
@@ -0,0 +1,12 @@
+export interface UploadResponse {
+  id: number;
+  filename: string;
+  originalname: string;
+  mimetype: string;
+  size: number;
+  type: string;
+  url: string;
+  cosKey: string;
+  createdAt: string;
+  updatedAt: string;
+}
