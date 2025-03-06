dbbcc50b51169f980c35af58f986e80356e56d1a
semyin <semyin@kplocker.com>
2025-03-06 15:50:38 +0800

    feat: file dto

--- server/file/dto/file.dto.ts
+++ server/file/dto/file.dto.ts
@@ -6,5 +6,32 @@ export class FileDto {
   @Expose()
   id!: number;
 
+  @Expose()
+  filename!: string;
 
+  @Expose()
+  originalname!: string;
+
+  @Expose()
+  mimetype!: string;
+
+  @Expose()
+  size!: number;
+
+  @Expose()
+  type!: string;
+
+  @Expose()
+  url!: string;
+
+  @Expose()
+  cosKey!: string;
+
+  @Expose()
+  @Transform(({ value }) => format(new Date(value), "yyyy-MM-dd HH:mm:ss")) // 转换为本地时间字符串
+  createdAt!: Date;
+
+  @Expose()
+  @Transform(({ value }) => format(new Date(value), "yyyy-MM-dd HH:mm:ss")) // 转换为本地时间字符串
+  updatedAt!: Date;
 }
